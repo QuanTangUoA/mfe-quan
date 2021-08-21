e3d2026573c194ca49eb1d475390697d26e7fac3
Quan Tang <quantang@Quans-MacBook-Pro.local>
2021-08-21 13:44:59 +0800

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import MarketingApp from '../components/MarketingApp';
-
+import Header from '../components/Header';
+import { BrowserRouter } from 'react-router-dom';
 export default () => {
-    return <div>
-        lolol000q3
-        <hr />
-        <MarketingApp />
-    </div>
+    return (
+        <BroswerRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BroswerRouter>
+    )
 }
